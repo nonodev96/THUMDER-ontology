eff51a7dc9b5aa3928fb2a57a47e92fedad12234
amm00220 <amm00220@red.ujaen.es>
2021-08-05 13:56:50 +0200

    first commit .idea

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { AgentAction } from "./Core/AgentAction";
+import { Concept } from "./Core/Concept";
+import { Predicate } from "./Core/Predicate";
+
+export {
+    AgentAction,
+    Concept,
+    Predicate
+}
